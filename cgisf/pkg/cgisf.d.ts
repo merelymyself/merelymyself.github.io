c495cfbe0a60a32cb6a7248a976b3f2cb077faaf
merelymyself <poonsters6@gmail.com>
2022-04-19 20:33:54 +0800

    gave up lul

--- cgisf/pkg/cgisf.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @returns {string}
-*/
-export function gen_sentence(): string;
--- cgisf/scripts.js
+++
@@ -1,8 +0,0 @@
-import {
-  gen_sentence
-  init
-} from './pkg/cgisf.js';
-init();
-let s = gen_sentence();
-function func() { document.getElementById("my_para").innerHTML = s; }
-document.getElementById("button").addEventListener("click", func(), false);
