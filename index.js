d7897692c9922829ba1c8083b9592e9446ec55ac
merelymyself <poonsters6@gmail.com>
2022-07-29 21:15:58 +0800

    add cgisf help

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ $(document).ready(function() {
 		}
 	});
 	$("#list1hover").click(function() {
-		help("#list1hover", "#list1hover2", "#list1", "#list1txt", "#list1txt2");
+		helpslide("#list1hover", "#list1hover2", "#list1", "#list1txt", "#list1txt2");
+	});
+	$("#list2hover").click(function() {
+		helptype("#list2txt");
 	});
 })
 
-function help(helpbutton, gobutton, bar, origmsg, newmsg) {
+function helpslide(helpbutton, gobutton, bar, origmsg, newmsg) {
 	if ($(bar).css("background-position") == "0px -100%") {
 		$(bar).css({ "background-position": "0% 0%", });
 		$(newmsg).css({"opacity":"0"});
@@ -39,5 +42,57 @@ function help(helpbutton, gobutton, bar, origmsg, newmsg) {
 		$(helpbutton).children().css({"color":"white"});
 		$(gobutton).css({"background-color":"#5E81AC"});
 		$(gobutton).children().css({"color":"white"});
+	}
+}
+
+function helptype(textbox) {
+	if ($(textbox).text() == "cgisf.") {
+		deleteString($(textbox), "|", 100,
+			function() {
+				typeString($(textbox), "A random sentence generator, ported to Javascript.", "|", 50);
+			}
+		);	
+	}
+	else if ($(textbox).text() == "A random sentence generator, ported to Javascript.") {
+		deleteString($(textbox), "|", 50,
+			function() {
+				typeString($(textbox), "cgisf.", "|", 100);
+			}
+		);	
+	}
+}
+function typeString($target, str, cursor, delay) {
+	let fixedtext = $target.text();
+	console.log(fixedtext);
+	if (fixedtext.endsWith(cursor)) {
+		fixedtext = fixedtext.slice(0, -1);
+	}
+	let string = fixedtext + str.charAt(fixedtext.length) + cursor;
+	$target.text(string);
+
+	if ($target.text().length < str.length + 1) {
+		setTimeout(function() {
+			typeString($target, str, cursor, delay);
+		}, delay);
+	}
+	else {
+		$target.text(string.slice(0, -1));
+	}
+}
+function deleteString($target, cursor, delay, cb) {
+	let fixedtext = $target.text();
+    console.log(fixedtext);
+ 	if (fixedtext.endsWith(cursor)) {
+		fixedtext = fixedtext.slice(0, -1);
+    }
+	fixedtext = fixedtext.slice(0, -1) + cursor;
+	$target.text(fixedtext);
+
+	if ($target.text().length > 1) {
+		setTimeout(function() {
+			deleteString($target, cursor, delay, cb);
+	  	}, delay);
+	} else {
+	  	cb();
 	}
 }
